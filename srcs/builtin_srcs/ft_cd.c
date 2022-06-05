a46647571184ec1a24edc3e404af2f05f8e4ed9a
gyuminv2 <inamemin3@gmail.com>
2022-06-06 00:44:08 +0900

    ft_exit

---
+++ srcs/builtin_srcs/ft_cd.c
@@ -0,0 +1,17 @@
+#include "../../includes/main.h"
+
+void	ft_cd(int ac, char *av[])
+{
+	//경로 이동에 대한 이해가 필요할듯요..
+	//getcwd, chdir
+
+	/*
+# . 현재 위치	cd ./
+
+# .. 부모 디렉토리	cd ../
+
+# 홈 디렉토리(환경변수의 HOME)	cd ~
+
+# 이전 디렉토리(환경변수의 OLDPWD)	cd - 
+											*/
+}
