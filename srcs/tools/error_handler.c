e74ec61bb6686e8ef1f0b0b6c40bf7f09ab4c5c5
Gyumin Park <gyumpark@c10r5s10.42seoul.kr>
2022-06-07 12:03:10 +0900

    justview

--- srcs/tools/error_handler.c
+++ srcs/tools/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: myukang <myukang@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 00:44:34 by myukang           #+#    #+#             */
-/*   Updated: 2022/06/07 00:48:35 by myukang          ###   ########.fr       */
+/*   Updated: 2022/06/07 12:02:16 by gyumpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,9 @@ void	print_error(int errnum)
 {
 	strerror(errnum);
 	exit(EXIT_FAILURE);
+}
+
+int main()
+{
+	print_error(1);
 }
