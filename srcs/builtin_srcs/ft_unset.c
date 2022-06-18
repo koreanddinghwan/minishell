21538a72398daa29055457c5af046a2c0fc13fce
gyuminv2 <inamemin3@gmail.com>
2022-06-19 00:00:34 +0900

    unset

--- srcs/builtin_srcs/ft_unset.c
+++ srcs/builtin_srcs/ft_unset.c
@@ -1,5 +1,31 @@
 #include "main.h"
 
+char	**remove_env_arr(t_data *data, char **args)
+{
+	char	**copy;
+	char	**split;
+	int i = 0;
+	int j = 0;
+
+	copy = (char **)malloc(sizeof(char *) * data->env_size * 10);
+	while (data->env[i])
+	{
+		split = ft_split(data->env[i], '=');
+		printf("arr: %s\nspl: %s\n", data->env[i], *args);
+		if (!ft_strcmp(split[0], *args))
+		{
+			data->env++;
+			args++;
+			continue;
+		}
+		copy[j] = ft_strdup(data->env[i]);
+		j++;
+		i++;
+	}
+	copy[j] = 0;
+	return	(copy);
+}
+
 void	free_env_lst(t_data *data, char **args)
 {
 	t_envlst	*rem_node;
@@ -23,9 +49,13 @@ void	free_env_lst(t_data *data, char **args)
 
 void	ft_unset(t_data *data, char **args)
 {
+	char		**copy;
+
+	copy = args;
 	while(*args)
 	{
 		free_env_lst(data, args);
 		args++;
 	}
+	data->env = remove_env_arr(data, copy);
 }
