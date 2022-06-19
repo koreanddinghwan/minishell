cfbda275499b68382bc19d27b4cde88c59cef1e2
gyuminv2 <inamemin3@gmail.com>
2022-06-19 16:43:02 +0900

    ㅅㅂ

--- srcs/builtin_srcs/ft_env.c
+++ srcs/builtin_srcs/ft_env.c
@@ -1,33 +1,20 @@
-#include "../../includes/main.h"
+#include "main.h"
 
-char	*make_key(char	*envstr)
+void    ft_env(t_data   *data)
 {
-	char	*str;
+    t_envlst    *lst;
+    int         i;
 
-	str = ft_strdup(envstr);
-	return (str);
-}
-
-void	set_env_key(t_data	*data, char	**envp)
-{
-	t_dlst	*node;
-
-	while (*envp)
-	{
-		node = ft_dlst_new(make_key(*envp));
-		ft_dlst_pushback(&data->env_lst, node);
-		printf("%s\n", (char *)data->env_lst->content);
-		envp++;
-		data->env_lst = data->env_lst->next;
-	}
-}
-
-int	main(int ac, char **av, char **envp)
-{
-	t_data	data;
-
-	(void)ac;
-	(void)av;
-	set_env_key(&data, envp);
-	return (0);
+    lst = data->env_lst;
+    i = 0;
+    while (lst)	//lst
+		{
+			printf("list: %s\n", (char*)lst->env_line);
+			lst = lst->next;
+		}
+	while (data->env[i])	//arr
+		{
+			printf("arr: %s\n", data->env[i]);
+			i++;
+		}
 }
--- srcs/builtin_srcs/redirection.c
+++ srcs/builtin_srcs/redirection.c
@@ -2,43 +2,142 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
-int	input_redirection(char *command, char *target, char **envp) // ex) cat, test.txt
-{
-	int	fd;
-	char **ag;
+// int	input_redirection(char *command, char *target, char **envp) // ex) cat < test.txt
+// {
+// 	int	fd;
+// 	char **ag;
+// 	pid_t	pid;
+// 	int		status;
 
-	ag = (char **)malloc(sizeof(char *) * 2);
-	ag[0] = "-e";
-	fd = open(target, O_RDONLY);
-	if (fd < 0)
-		return (-1);
-	dup2(fd, STDIN_FILENO);
-	// 빌트인인지 아닌지 확인
-	execve(command, ag, envp);
-	close(fd);
-	return (1);
-}
+// 	ag = (char **)malloc(sizeof(char *) * 2);
+// 	ag[0] = "-e";
+// 	fd = open(target, O_RDONLY);
+// 	pid = fork();
+// 	if (pid < 0)
+// 		exit(1);
+// 	if (fd < 0)
+// 		return (-1);
+// 	if (pid == 0)
+// 	{
+// 		printf("child 시작 pid : %ld\n", (long)getpid());
+// 		dup2(fd, STDIN_FILENO);
+// 		// 빌트인인지 아닌지 확인
+// 		execve(command, ag, envp);
+// 	}
+// 	else
+// 	{
+// 		printf("부모 pid : %ld, pid : %d\n",(long)getpid(), pid);
+// 		close(fd);
+// 		waitpid(pid, &status, 0);
+// 		printf("child 종료\n");
+// 		printf("부모 종료 %d %d %d\n", pid, WIFEXITED(status), WEXITSTATUS(status)); // 1, 0(자식)
+// 	}
+// 	return (1);
+// }
 
-int output_redirection()
+// int output_redirection(char *command, char *target, char **envp)
+// {
+// 	int fd;
+// 	pid_t pid;
+
+// 	char *ag[]={"/usr/bin", NULL};
+// 	fd = open(target, O_CREAT | O_RDWR | O_TRUNC , 0644);
+// 	pid = fork();
+// 	if (pid < 0)
+// 		exit(1);
+// 	if (fd < 0)
+// 		return (-1);
+// 	if (pid == 0)
+// 	{
+// 		printf("[cat > text.txt] start!\nStop: ctrl+c\n");
+// 		dup2(fd, STDOUT_FILENO);
+// 		printf("%d\n", execve(command, ag, envp));
+// 		printf("%s\n", strerror(errno));
+// 	}
+// 	else
+// 	{
+// 		waitpid(pid, NULL, 0);
+// 		close(fd);
+// 	}
+// 	return (1);
+// }
+
+// int double_output_redirection(char *command, char *target, char **envp)
+// {
+// 	int fd;
+// 	pid_t pid;
+
+// 	char *ag[]={"/usr/bin", NULL};
+// 	fd = open(target, O_CREAT | O_WRONLY | O_APPEND , 0644);
+// 	pid = fork();
+// 	if (pid < 0)
+// 		exit(1);
+// 	if (fd < 0)
+// 		return (-1);
+// 	if (pid == 0)
+// 	{
+// 		printf("[cat >> test.txt] start!\nStop: ctrl+c\n");
+// 		dup2(fd, STDOUT_FILENO);
+// 		printf("%d\n", execve(command, ag, envp));
+// 		printf("%s\n", strerror(errno));
+// 	}
+// 	else
+// 	{
+// 		waitpid(pid, NULL, 0);
+// 		close(fd);
+// 	}
+// 	return (1);
+// }
+
+int	here_document()
 {
 	int fd;
-	
-	fd = open(target, O_CREAT | O_WRONLY | O_TRUNC ,0644);
-	if (fd < 0)
-		return (-1);
-	dup2(fd, STDIN_FILENO);
-	close(fd);
+	pid_t pid;
+	char *line;
+	char *ag[]={"/bin/cat", NULL};
+
+	fd = open("hi", O_CREAT | O_RDWR | O_TRUNC, 0644);
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("[cat << hi]: start!\n");
+		dup2(fd, STDIN_FILENO);
+		execve("bin/cat", ag, 0);
+		while (1)
+		{
+			line = readline("heredoc> ");
+			if (!strcmp(line, "hi"))
+			{
+				printf(" exit\n");
+				return (0);
+			}
+		}
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+		close(fd);
+	}
 	return (1);
 }
 
 int main(int ac, char **av, char **envp)
 {
 	// input_redirection: '<' ex) cat < test.txt == < test.txt cat
-	// char cat[9] = "/bin/cat";
-	// char tar[9] = "test.txt";
-	// input_redirection(cat, tar, envp);
-
+	char cat[9] = "/bin/cat";
+	char echo[10] = "/bin/echo";
+	char env[12] = "/usr/bin/env"; // not a directory: ?????????
+	char tar[9] = "test.txt";
+	// input_redirection(env, tar, envp);
+	// output_redirection(cat, tar, envp);
+	// double_output_redirection(cat, tar, envp);
+	here_document();
 	// here document: '<<'
 
 	// output_redirection: '>' ex) echo abc > test.txt == > test.txt echo abc
