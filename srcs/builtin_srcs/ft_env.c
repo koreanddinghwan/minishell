e5a6b57fe5b4839b5f335f867a851a5307171408
gyuminv2 <inamemin3@gmail.com>
2022-06-19 17:01:26 +0900

    TLqkf

--- srcs/builtin_srcs/ft_env.c
+++ srcs/builtin_srcs/ft_env.c
@@ -3,15 +3,15 @@
 void    ft_env(t_data   *data)
 {
     t_envlst    *lst;
-    int         i;
 
     lst = data->env_lst;
-    i = 0;
     while (lst)	//lst
 		{
 			printf("list: %s\n", (char*)lst->env_line);
 			lst = lst->next;
 		}
+    // int         i;
+    // i = 0;
 	// while (data->env[i])	//arr
 	// 	{
 	// 		printf("arr: %s\n", data->env[i]);
