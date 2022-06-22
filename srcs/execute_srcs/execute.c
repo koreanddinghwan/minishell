7e299157fc0039a2919ea129b33f3eed5e640b76
Gyumin Park <gyumpark@c10r5s10.42seoul.kr>
2022-06-22 20:15:42 +0900

    Feat: execute test

--- srcs/execute_srcs/execute.c
+++ srcs/execute_srcs/execute.c
@@ -37,16 +37,20 @@ int output_redirection(t_data *data, char *command, char *target, char **args, c
 	(void) command;
 	int fd;
 	pid_t pid;
+	int status;
 
 	// char *ag[]={"/usr/bin", NULL};
 	fd = open(target, O_CREAT | O_RDWR | O_TRUNC , 0644);
 	pid = fork();
 	if (pid < 0)
 		exit(1);
-	if (fd < 0)
-		return (-1);
 	if (pid == 0)
 	{
+		if (fd < 0)
+		{
+			printf("%s\n", strerror(errno));
+			return (-1);
+		}
 		printf("[cat > text.txt] start!\nStop: ctrl+c\n");
 		dup2(fd, STDOUT_FILENO);
 		ft_env(data);
@@ -56,7 +60,8 @@ int output_redirection(t_data *data, char *command, char *target, char **args, c
 	}
 	else
 	{
-		waitpid(pid, NULL, 0);
+		waitpid(pid, &status, 0);
+		printf("status: %d\n", status);
 		close(fd);
 	}
 	return (1);
@@ -123,134 +128,94 @@ void	execute_builtin(t_data *data, char *cmd, char **args)
 	// 	input_redirection();
 	// }
 
-	// if (ft_strcmp(ENV, cmd) == 0)
-	// 	ft_env(data);
+	if (ft_strcmp(ENV, cmd) == 0)
+		ft_env(data);
+}
+
+void	set_pipe(int pipefd[2], t_data *data, char **args)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pipe(pipefd) == -1)
+	{
+		printf("%s\n", strerror(errno));
+		return ;
+	}
+	// if (no_builtin)
+	// {
+		if (pid == 0)
+		{
+			close(pipefd[1]);
+			dup2(pipefd[0], STDIN_FILENO);
+			ft_cd(data, args);
+			close(pipefd[0]);
+		}
+		else
+		{
+			close(pipefd[0]);
+			dup2(pipefd[1], STDOUT_FILENO);
+			ft_env(data);
+			close(pipefd[1]);
+		}
+	// }
+	// dup2(pipefd[1], STDOUT_FILENO);
 }
 
 void	execute(t_data *data)
 {
 	t_dlst *cmd_lst;
 	cmd_lst = data->cmd_lst;
+	int pipefd[2];
 
-	// // t_envlst	*lst;
-	// t_dlst		*cmd_lst;
-
-	// // lst = data->env_lst;
-	// cmd_lst = data->cmd_lst;
-	char *cmd = GET_CMD(cmd_lst);
+	// char *cmd = GET_CMD(cmd_lst);
 	char **args = GET_ARGS(cmd_lst);
-
 	// if (파이프X)
 	// {
 	// 	단일커맨드 처리
 	// }
 	// else (파이프O)
 	// {
-
+		set_pipe(pipefd, data, args);
+		printf("pipefd: %d, %d\n", pipefd[0], pipefd[1]);
+		// if (builtin(cmd))
+		// 	execute_builtin(data, cmd, args);
 	// }
 
-	if (builtin(cmd))
-		execute_builtin(data, cmd, args);
 	// else if (!builtin(commd))
 	// 	execute_execve();
 	cmd_lst = cmd_lst->next;
 	data->cmd_lst = data->cmd_lst->next;
-
-
-
-	// if (!strcmp(data->command, ENV))
-	// {
-	// 	while (lst)	//lst
-	// 	{
-	// 		printf("list: %s\n", (char*)lst->env_line);
-	// 		lst = lst->next;
-	// 	}
-	// 	while (data->env[i])	//arr
-	// 	{
-	// 		printf("arr: %s\n", data->env[i]);
-	// 		i++;
-	// 	}
-	// }
-	// if (!strcmp(data->command, CD))
-	// {
-	// 	ft_cd(data, "~");
-	// }
-	// if (!strcmp(data->command, PWD))
-	// {
-	// 	ft_pwd();
-	// }
-	// /*
-	// if (!strcmp(data->command, EXIT))
-	// {
-	// 	ft_exit(ac, av); // 수정: ac는 exit 이후의 av개수(파싱이후), 지금은 exit만 됌
-	// }
-	// */
-	// if (!strcmp(data->command, EXPORT))
-	// {
-	// 	char **args = (char **)malloc(sizeof(char *) * 3);
-	// 	args[0] = (char *)malloc(sizeof(char) * 10);
-	// 	args[1] = (char *)malloc(sizeof(char) * 10);
-	// 	args[2] = (char *)malloc(sizeof(char) * 10);
-	// 	// args[0] = "key=value";
-	// 	// args[1] = "value=key";
-	// 	// args[0] = "key";
-	// 	// args[1] = "value";
-	// 	args[0] = "key=";
-	// 	args[1] = "value=";
-	// 	args[2] = 0;
-	// 	ft_export(data, args);
-	// }
-	// if (!strcmp(data->command, UNSET))
-	// {
-	// 	char **args = (char **)malloc(sizeof(char *) * 3);
-	// 	args[0] = (char *)malloc(sizeof(char) * 10);
-	// 	args[1] = (char *)malloc(sizeof(char) * 10);
-	// 	args[2] = (char *)malloc(sizeof(char) * 10);
-	// 	args[0] = "key";
-	// 	args[1] = "value";
-	// 	args[2] = 0;
-	// 	ft_unset(data, args);
-	// }
-	// if (!strcmp(data->command, ECHO))
-	// {
-	// 	int flag = 0; // no option
-	// 	//flag = 1; // (-n) option
-	// 	if (flag == 0)
-	// 		ft_echo(data, flag, "HI");
-	// 	else
-	// 		ft_echo(data, flag, "-nnn HI");
-	// }
 }
+// no_builtin()
+// {
+// 	pid_t pid;
+// 	pid = fork();
+// 	if (pid == 0)
+// 	{
+// 		if (pipe_exist)
+// 			execute_pipe();
+// 		else
+// 			execve();
+// 		exit($?);
+// 	}
+// 	else
+// 	{
+// 		waitpid();
+// 		close(fd);
+// 	}
+// }
 
-no_builtin()
-{
-	pid_t pid;
-	pid = fork();
-	if (pid == 0)
-	{
-		if (pipe_exist)
-			execute_pipe();
-		else
-			execve();
-		exit($?);
-	}
-	else
-	{
-		waitpid();
-		close(fd);
-	}
-}
+// example()
+// {
+// 	if (builtin)
+// 		builtin_redirect_function();
+// 	else
+// 		no_builtin();
+// }
 
-example()
-{
-		if (builtin)
-			builtin_redirect_function();
-		else
-			no_builtin();
-}
-
-echo 123 > test.txt | cat test.txt
-builtin_redirect_function()
-pipe_exist()
-no_builtin()
-fork()
+// echo 123 > test.txt | cat test.txt
+// builtin_redirect_function()
+// pipe_exist()
+// no_builtin()
+// fork()
