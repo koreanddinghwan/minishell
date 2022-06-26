ef5f7d7de649a95f8f8b1b58f76835927740284b
Gyumin Park <gyumpark@c10r5s10.42seoul.kr>
2022-06-26 20:08:23 +0900

    "FEAT: test.c(파이프)"

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void    execve_com(char *com, int fd[2], int flag, char **envp)
+void    execve_com(char *com, int fd[2][2], int flag, char **envp, int i)
 {
         pid_t pid;
         pid = fork();
@@ -14,24 +14,43 @@ void    execve_com(char *com, int fd[2], int flag, char **envp)
 
         if (pid == 0)
         {
-                // close(fd[0]);
-                printf("dup2(OUT)\n");
-                dup2(fd[1], STDOUT_FILENO);
+                if (i == 0)
+                {
+                        close(fd[0][0]);
+                        close(fd[1][0]);
+                        close(fd[1][1]);
+                        dup2(fd[0][1], STDOUT_FILENO);
+                }
+                if (i == 1)
+                {
+                        close(fd[1][0]);
+                        close(fd[0][1]);
+                        dup2(fd[0][0], STDIN_FILENO);
+                        dup2(fd[1][1], STDOUT_FILENO);
+                }
+                if (i == 2)
+                {
+                        close(fd[1][1]);
+                        close(fd[0][0]);
+                        close(fd[0][1]);
+                        dup2(fd[1][0], STDIN_FILENO);
+                }       
                 execve(com, ag, envp);
         }
         // waitpid(pid, 0, 0);
-        if (flag == 0)
-        {
-                // close(fd[1]);
-                printf("dup2(IN)\n");
-                dup2(fd[0], STDIN_FILENO);
+        // if (flag == 0)
+        // {
+        //         close(fd[1]);
+        //         printf("dup2(IN)\n");
+        //         dup2(fd[0], STDIN_FILENO);
+        //         close(fd[0]);
                 
-        }
-        else if (flag == 1)
-        {
-                close(fd[1]);
-                close(fd[0]);
-        }
+        // }
+        // else if (flag == 1)
+        // {
+        //         close(fd[1]);
+        //         close(fd[0]);
+        // }
 }
 
 int main(int ac, char *av[], char **envp)
@@ -43,35 +62,39 @@ int main(int ac, char *av[], char **envp)
         char env[] = "/usr/bin/env";
         char head[] = "/usr/bin/head";
         char pwd[] = "/bin/pwd";
-        int fd[2];
-        int n_fd[2];
+        int     fd_pipes[2][2];
         int status;
         // int pipe_flag = 1;
         int pipe_flag = 0;
+        int     i;
 
-        pipe(fd);
-        // pipe(n_fd);
+        pipe(fd_pipes[0]);
+        pipe(fd_pipes[1]);
 
-        execve_com(pwd, fd, pipe_flag, envp);           // STDIN이 들어오는 명령어는 dup2 STDIN_FILENO를 쓰면 안됌
-        // --pipe_flag;                                    // STDIN이 들어오지않는 명령어는 dup2사용
+        i = 0;
+        execve_com(head, fd_pipes, pipe_flag, envp, i);           // STDIN이 들어오는 명령어는 dup2 STDIN_FILENO를 쓰면 안됌
+        ++pipe_flag;                                    // STDIN이 들어오지않는 명령어는 dup2사용
         printf("close: fd[1]\n");
-        close(fd[1]);
+        // close(fd[1]);
 
         // execve_com(ls, n_fd, pipe_flag, envp);
         // ++pipe_flag;
         // close(n_fd[0]);
-        
-        execve_com(pwd, fd, pipe_flag, envp);
+        i =1;
+        execve_com(cat, fd_pipes, pipe_flag, envp, i);
         printf("close: fd[0]\n");
-        close(fd[0]);
-        printf("%d %d\n", fd[0], fd[1]);
-
-        // int t_fd[2] = {fd[0], n_fd[1]};
-        // execve_com(wc, t_fd, pipe_flag, envp);
         // close(fd[0]);
-        // close(n_fd[1]);
-        while (wait(&status) > 0)
-        {
-                printf("-------------------\n");
-        }
+        i = 2;
+        execve_com(ls, fd_pipes, pipe_flag, envp, i);
+
+        close(fd_pipes[0][0]);
+        close(fd_pipes[0][1]);
+        close(fd_pipes[1][0]);
+        close(fd_pipes[1][1]);
+        wait(&status);
+        printf("%d\n", status);
+        wait(&status);
+        printf("%d\n", status);
+        wait(&status);
+        printf("%d\n", status);
 }
