2002e3eeb1f51a542477edaacc2108d67bf66fc0
myukang <myunghwan0421@gmail.com>
2022-06-27 23:08:25 +0900

    FEAT:replacer norm

--- srcs/module/replacer_module/replacer.c
+++ srcs/module/replacer_module/replacer.c
@@ -6,16 +6,9 @@
 /*   By: myukang <myukang@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 02:04:36 by myukang           #+#    #+#             */
-/*   Updated: 2022/06/27 15:47:23 by myukang          ###   ########.fr       */
+/*   Updated: 2022/06/27 18:30:32 by myukang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
- * replacer module is used in lexer,
- * replacer do
- * 1. trim \" and \'
- * 2. if buffer is not start with \'
- *    => replace $? and $ENV with exit_status and env variable
- * */
 
 #include "replacer.h"
 
@@ -39,21 +32,23 @@ char	*get_replaced(t_data *data, char *str)
 void	replacer_exec(t_data *data, t_dlst *tok_lst)
 {
 	char	*buffer;
+	char	*dq;
+	char	*sq;
 
 	buffer = get_ltok_cont(tok_lst)->buffer;
 	if (buffer[0] == ' ')
 		return ;
 	if (buffer[0] == '\'')
 	{
-		get_ltok_cont(tok_lst)->buffer = ft_strtrim(buffer, "\'");
+		(get_ltok_cont(tok_lst))->buffer = ft_strtrim(buffer, "\'");
 		free(buffer);
 	}
 	else
 	{
-		get_ltok_cont(tok_lst)->buffer = ft_strtrim(buffer, "\"");
+		(get_ltok_cont(tok_lst))->buffer = ft_strtrim(buffer, "\"");
 		free(buffer);
 		buffer = get_ltok_cont(tok_lst)->buffer;
-		get_ltok_cont(tok_lst)->buffer = get_replaced(data, buffer);
+		(get_ltok_cont(tok_lst))->buffer = get_replaced(data, buffer);
 	}
 }
 
