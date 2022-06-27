856e66f021638a393722a69b46ff2bafb4643df5
myukang <myunghwan0421@gmail.com>
2022-06-27 23:10:04 +0900

    NORM:replacer

--- srcs/module/replacer_module/replacer.c
+++ srcs/module/replacer_module/replacer.c
@@ -6,7 +6,7 @@
 /*   By: myukang <myukang@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 02:04:36 by myukang           #+#    #+#             */
-/*   Updated: 2022/06/27 18:30:32 by myukang          ###   ########.fr       */
+/*   Updated: 2022/06/27 23:09:47 by myukang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,6 @@ char	*get_replaced(t_data *data, char *str)
 void	replacer_exec(t_data *data, t_dlst *tok_lst)
 {
 	char	*buffer;
-	char	*dq;
-	char	*sq;
 
 	buffer = get_ltok_cont(tok_lst)->buffer;
 	if (buffer[0] == ' ')
