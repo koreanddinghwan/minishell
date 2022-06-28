7161305d213399fa6cf5624173ee1a6326ef5ca1
myukang <myunghwan0421@gmail.com>
2022-06-28 13:53:19 +0900

    "FIX":"ls $없는 환경변수"

--- srcs/module/replacer_module/replacer.c
+++ srcs/module/replacer_module/replacer.c
@@ -6,7 +6,7 @@
 /*   By: myukang <myukang@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 02:04:36 by myukang           #+#    #+#             */
-/*   Updated: 2022/06/28 03:24:58 by myukang          ###   ########.fr       */
+/*   Updated: 2022/06/28 13:51:51 by myukang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ char	*get_replaced(t_data *data, char *str)
 	int		len;
 
 	len = get_replaced_len(str, data);
-	ft_printf("rep len%d\n", len);
+	if (len == 0)
+		return (NULL);
 	rtn = malloc(sizeof(char) * (len + 1));
 	if (!rtn)
 	{
