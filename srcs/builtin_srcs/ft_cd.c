bab64456f7462e03557767c5aad72968024a6452
gyuminv2 <inamemin3@gmail.com>
2022-06-30 02:43:10 +0900

    FIX: cd

--- srcs/builtin_srcs/ft_cd.c
+++ srcs/builtin_srcs/ft_cd.c
@@ -13,92 +13,69 @@
 #include "main.h"
 #include <errno.h>
 
-void	insert_oldpwd(t_data *data)
-{
-	t_envlst	*new;
-
-	new = ft_envlst_new("OLDPWD=");
-	ft_envlst_pushback(&data->env_lst, new);
-}
-
-void	change_env(t_data *data, char *pwd, char *oldpwd)
+void	change_env(t_data *data, char *oldpwd)
 {
 	t_envlst	*node;
 	t_envlst	*last;
-	int			flag;
-
+	char		*pwd;
+	
 	node = data->env_lst;
 	last = ft_envlst_last(node);
-	flag = 0;
+	pwd = (char *)malloc(sizeof(char) * 256);
+	pwd = getcwd(pwd, 256);
 	while (node)
 	{
-		if (ft_strcmp(node->key, "PWD") == 0)
+		if (!ft_strncmp(node->key, "PWD", 3))
 		{
 			free(node->value);
-			node->value = ft_strdup(pwd);
+			node->value = pwd;
+			if (ft_strcmp(last->key, "OLDPWD"))
+			{
+				node = ft_envlst_new("OLDPWD=OLDPWD");
+				ft_envlst_pushback(&data->env_lst, node);
+			}
 		}
-		if (ft_strcmp(node->key, "OLDPWD") == 0)
+		if (!ft_strncmp(node->key, "OLDPWD", 6))
 		{
-			flag = 1;
 			free(node->value);
-			node->value = ft_strdup(oldpwd);
+			node->value = oldpwd;
 		}
 		node = node->next;
 	}
-	if (flag == 0)
-		insert_oldpwd(data);
 }
-
-void	change_dir_env(t_data *data, char *buf, char *aft, char *cur)
-{
-	int	change;
-
-	change = chdir(buf);
-	if (change == -1)
-		printf("bash: cd: %s\n", strerror(errno));
-	else
-	{
-		getcwd(aft, 256);
-		change_env(data, aft, cur);
-	}
-	free(buf);
-}
-
-char	*get_chdir_buf(char **path, char *home, char *old_save)
-{
-	char	*buf;
-
-	if (!*path)
-		buf = ft_strdup(home);
-	else if (!ft_strcmp(*path, "~"))
-		buf = ft_strdup(home);
-	else if (!ft_strcmp(*path, "-"))
-		buf = ft_strdup(old_save);
-	else
-		buf = ft_strdup(*path);
-	return (buf);
-}
-
 void	ft_cd(t_data *data, char **path)
 {
-	char		aft[256];
-	char		cur[256];
-	char		*home;
-	char		*old_save;
 	t_envlst	*node;
+	char	*buf;
+	char	*home;
+	char	*old_save;
+	char	*cur;
 
 	path++;
 	node = data->env_lst;
-	getcwd(cur, 256);
-	old_save = 0;
-	while (node)
+	cur = (char *)malloc(sizeof(char) * 256);
+	cur = getcwd(cur, 256);
+	while(node)
 	{
-		if (!ft_strcmp(node->key, "HOME"))
+		if(!ft_strcmp(node->key, "HOME"))
 			home = node->value;
 		if (!ft_strcmp(node->key, "OLDPWD"))
-			old_save = node->value;
-		node = node->next;
+ 			old_save = node->value;
+ 		node = node->next;
+ 	}
+ 	if (!*path)
+ 		buf = ft_strdup(home);
+ 	else if (!strcmp(*path, "~"))
+ 		buf = ft_strdup(home);
+ 	else if (!strcmp(*path, "-"))
+ 		buf = ft_strdup(old_save);
+	else
+		buf = ft_strdup(*path);
+	if (chdir(buf) == -1)
+		printf("mgyush: cd: %s\n", strerror(errno));
+	else
+	{
+		change_env(data, cur);
 	}
-	change_dir_env(data, get_chdir_buf(path, home, old_save), aft, cur);
-	update_env_arr(data);
+	free(buf);
 }
