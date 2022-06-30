4bdd570e16ac3820f2bb93f5a24bd7dabb707953
Myunghwan Kang <myukang@c10r4s13.42seoul.kr>
2022-06-30 16:51:55 +0900

    echo test

--- srcs/builtin_srcs/ft_echo.c
+++ srcs/builtin_srcs/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: myukang <myukang@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 15:43:39 by myukang           #+#    #+#             */
-/*   Updated: 2022/06/30 13:35:14 by myukang          ###   ########.fr       */
+/*   Updated: 2022/06/30 16:46:35 by myukang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ void	print_echo(char **argv, int no_opt_i, int argv_count)
 	int	option;
 
 	i = 0;
-	if (no_opt_i < argv_count)
+	if (no_opt_i == argv_count)
 		option = FALSE;
 	else
 		option = TRUE;
@@ -75,9 +75,9 @@ void	print_echo(char **argv, int no_opt_i, int argv_count)
 	{
 		write(1, argv[no_opt_i], ft_strlen(argv[no_opt_i]));
 		i++;
+		no_opt_i++;
 		if (argv[no_opt_i])
 			write(1, " ", 1);
-		no_opt_i++;
 	}
 	if (option == FALSE)
 		write(1, "\n", 1);
